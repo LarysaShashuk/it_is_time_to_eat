757d6893817a503c929e079a12d68d3cef89fecd
Larysa Shashuk <larisa.shashuk@gmail.com>
2021-05-06 18:36:38 +0300

    add cat paws

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const button = document.querySelector('.button');
 const fireworksArr = document.querySelectorAll('.firework');
+const pawsArr = document.querySelectorAll('.paw');
 
 button.addEventListener('click', () => {
   fireworksArr.forEach((element) => {
@@ -7,4 +8,18 @@ button.addEventListener('click', () => {
       ? element.classList.remove('hidden')
       : element.classList.add('hidden');
   });
+
+  let i = 0;
+  function callMe() {
+    const timetowait = 1000;
+    pawsArr[i].classList.contains('shownPaw')
+      ? pawsArr[i].classList.remove('shownPaw')
+      : pawsArr[i].classList.add('shownPaw');
+    i++;
+    if (i < pawsArr.length) {
+      setTimeout(callMe, timetowait);
+    }
+  }
+
+  callMe();
 });
